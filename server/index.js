b8e782162689c6904084d61503429d8522d9980e
hanga.lagoue <hanga.lagoue@lao-sarl.cm>
2022-07-25 00:05:34 +0100

    aurelien-quiz:main: Implement Quiz App Both Frontend And Backend Sides Using Mongodb Database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config({ path: './env' });
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect(process.env.DB_URI, {
+mongoose.connect("mongodb+srv://juniorlagoue:deuxmille@cluster0.aqlza.mongodb.net/quiz?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
