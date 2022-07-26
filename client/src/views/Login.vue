0da134cc888fedb2b8f49f5908cfca4c396e1ebd
hanga.lagoue <hanga.lagoue@lao-sarl.cm>
2022-07-26 23:40:36 +0100

    restart the build

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full max-w-xs mx-auto translate-y-1/2">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-slate-100" @submit.prevent="goToHomePage">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-slate-100">
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
           Username
@@ -34,7 +34,7 @@
                v-model="user.firstname"
         >
       </div>
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between" @click.prevent="goToHomePage">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
           Sign In
         </button>
